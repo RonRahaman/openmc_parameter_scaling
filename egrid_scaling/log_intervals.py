99838a7b669c183c0033982e56d0dcbbbb4496c1
Ron Rahaman <rahaman@cs.uchicago.edu>
2014-09-05 15:36:34 -0500

    started log_intervals for finding resize egrid values

--- egrid_scaling/log_intervals.py
+++ egrid_scaling/log_intervals.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python env
+#!/usr/bin/env python
 import numpy as np
 
 def log_intervals(left, right, n):
@@ -8,3 +8,31 @@ def log_intervals(left, right, n):
     for i in range(1,n):
         result[i] = result[i-1]*interval
     return result
+
+if __name__ == '__main__':
+    left = 2.4
+    right = 215.
+    n = 16.
+    interval = np.exp( (np.log(right) - np.log(left)) / (n-1) )
+
+    neg = [left]
+    ueg = []
+
+    while True:
+        x = neg[-1]*interval 
+        if x < 215:
+            neg.append(x)
+            if x >= 67.9:
+                ueg.append(x)
+        else:
+            break
+
+    while True:
+        x = ueg[-1]*interval
+        if x < 1584:
+            ueg.append(x)
+        else:
+            break
+
+    print neg
+    print ueg
