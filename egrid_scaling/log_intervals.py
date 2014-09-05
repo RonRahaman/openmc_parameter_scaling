630a672dcf6928bc92c4173e08d8ecad78edee84
Ron Rahaman <rahaman@cs.uchicago.edu>
2014-09-05 11:13:16 -0500

    worked on materialials plotting; fixed band intervals

--- egrid_scaling/log_intervals.py
+++ egrid_scaling/log_intervals.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def log_intervals(left, right, n):
     result = np.empty(n)
-    interval = np.exp( (np.log(right) - np.log(left) + 1) / n )
+    interval = np.exp( (np.log(right) - np.log(left)) / (n-1) )
     result[0] = left
     for i in range(1,n):
         result[i] = result[i-1]*interval
